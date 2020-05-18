993d5a5f138f7022904864835f107a2419e20e16
PriyankaKulkarni14 <40451419+PriyankaKulkarni14@users.noreply.github.com>
2020-05-18 13:23:21 +0530

    Add files via upload

---
+++ env_var.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Setting environment variables for Terraform"
+export ARM_SUBSCRIPTION_ID=a5ebbd9d-f249-4493-b82c-9d4a71848b67
+export ARM_CLIENT_ID=e2ef5018-4b6f-4c6e-8572-f1e8db742d39
+export ARM_CLIENT_SECRET=8f66bdb4-ce7b-47f3-a925-fc56ca961821
+export ARM_TENANT_ID=7a04c31d-3184-4c41-b14c-848acc94c514
